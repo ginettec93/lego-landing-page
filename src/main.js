c9ea0f7fb0c7b0613dd57fbb98225a4c2c2aee8c
ginettec93 <ginettecarrizo@gmail.com>
2023-05-25 18:33:29 -0300

    se han aplicado estilos y los eventos en JavaScript.

--- src/main.js
+++ src/main.js
@@ -1,22 +1,14 @@
 const modal = document.querySelector('.modal');
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector('#button2');
-const button3 = document.querySelector('#button3');
+const allButtonImages = document.querySelectorAll('.buttonImage');
 const closeButton = document.querySelector('.close-button');
 
-button1.addEventListener('click', () => {
-    modal.classList.remove('hidden')
-    modal.classList.add('visible')
-})
-button2.addEventListener('click', () => {
-    modal.classList.remove('hidden')
-    modal.classList.add('visible')
-})
-button3.addEventListener('click', () => {
-    modal.classList.remove('hidden')
-    modal.classList.add('visible')
+allButtonImages.forEach(simpleButton => {
+    simpleButton.addEventListener('click', () => {
+        modal.classList.remove('hidden')
+        modal.classList.add('visible')
+    })
 })
 closeButton.addEventListener('click', () => {
     modal.classList.add('hidden')
     modal.classList.remove('visible')
-} )
+})
